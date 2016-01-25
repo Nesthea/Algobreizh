0e7e40121954c973b8ba88df20f7a26b164c1fbe
Konosprod <konosprod@free.fr>
2016-01-25 14:58:37 +0100

    Mise à jour de lib.php et affichage du nom du produit

--- lib/lib.php
+++ lib/lib.php
@@ -7,7 +7,6 @@ function createConnexion()
 
 function getLastOrders($codeClient)
 {
-	/*
 	$db = createConnexion();
 	
 	$sql = "select * from commandes where codeClient = :codeClient order by dateCommande limite 20";
@@ -17,14 +16,11 @@ function getLastOrders($codeClient)
 	if($stmt->execute(array("codeClient"=>$codeClient)))
 	{
 		return $stmt->fetchAll(PDO::FETCH_ASSOC);
-	}*/
-	
-	return array(0 => array("idCommande" => 1, "dateCommande" => "01/01/1995", "montant" => 45.45));
+	}
 }
 
 function getLastBills($codeClient)
 {
-	/*
 	$db = createConnexion();
 	
 	$sql = "";
@@ -34,14 +30,11 @@ function getLastBills($codeClient)
 	if($stmt->execute(array("codeClient"=>$codeClient)))
 	{
 		return $stmt->fetchAll(PDO::FETCH_ASSOC);
-	}*/
-	
-	return array(0 => array("idCommande" => 1, "dateCommande" => "01/01/1995", "montant" => 45.45, "valide"=>0, "codeClient" => 24));
+	}
 }
 
 function getOrderItems($idCommand)
 {
-	/*
 	$db = createConnexion();
 
 	$sql = "";
@@ -49,36 +42,40 @@ function getOrderItems($idCommand)
 	if($stmt->execute(array("codeClient"=>$codeClient)))
 	{
 		return $stmt->fetchAll(PDO::FETCH_ASSOC);
-	}*/
-	
-	return array(0 => array("codeArticle"=>1, "qteArticle"=>1, "montant"=>4.45, "idCommand"=>$idCommand),
-				 1 => array("codeArticle"=>1, "qteArticle"=>1, "montant"=>4.45, "idCommand"=>$idCommand),
-				 2 => array("codeArticle"=>1, "qteArticle"=>1, "montant"=>4.45, "idCommand"=>$idCommand));
+	}
 }
 
 function getItemInfo($codeArticle)
 {
-	/*
 	$db = createConnexion();
 	
 	$sql = "";
 	
 	if($stmt->execute(array("codeClient"=>$codeClient)))
 	{
-	return $stmt->fetchAll(PDO::FETCH_ASSOC);
-	}*/
-
-	return array("codeArticle"=>$codeArticle, "libelleArticle"=>"patate", "prix"=>4, "unite"=>"Sachet de 50g");
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+	}
 }
 
 function getLibelleArticle($idArticle)
 {
-	return "blah";
+	$db = createConnexion();
+	
+	$sql = "select * from articles where idArticle = :idArticle";
+	
+	$stmt = $db->prepare($sql);
+	
+	if($stmt->execute(array("idArticle" => $idArticle)))
+	{
+		return $stmt->fetch(PDO::FETCH_ASSOC);
+	}
+	
+	return $idArticle;
 }
 
 function getUnprocessedOrders()
 {
-	//$db = createConnexion();
+	$db = createConnexion();
 	
 	return array(0 => array("idCommande" => 1, "dateCommande" => "01/01/1995", "montant" => 45.45, "codeClient" => 24));
 }
